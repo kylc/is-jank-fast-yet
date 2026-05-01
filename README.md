b1086d8e2d2b36fcdf70545399d12e4a4d49a45b
Kyle Cesare <kcesare@gmail.com>
2026-05-01 15:27:50 -0600

    Make run script configurable.

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # is-jank-fast-yet
 
-## TODO
+Tracks relative runtime performance of Clojure dialects over time.
 
-- Add additional benchmarks
-- Use a more sophisticated benchmark harness (ex. criteron on Clojure)
+## Usage
+
+Run the benchmark suite and produce an HTML output:
+
+``` sh
+./update.sh --run
+```
+
+Run using an alternative jank source:
+
+``` sh
+# Local checkout
+./update.sh --run --source /path/to/jank-git
+
+# Remote branch
+./update.sh --run --source git+https://github.com/jank-lang/jank?ref=some-branch
+```
+
+Afterwards, you can check the output `index.html` page for graphical results.
--- update.sh
+++ update.sh
@@ -1,28 +1,82 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
+
+RUN=false
+PUBLISH=false
+JANK_SOURCE="git+https://github.com/jank-lang/jank"
+
+usage() {
+	echo "Usage: $0 [OPTIONS]"
+	echo "  --run           Run the benchmarks (default: false)"
+	echo "  --publish       Publish to git (default: false)"
+	echo "  --source <name> Specify the jank git source (default: git+https://github.com/jank-lang/jank)"
+	echo "  --help          Display this help message"
+	exit 1
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	--run)
+		RUN=true
+		shift
+		;;
+	--publish)
+		PUBLISH=true
+		shift
+		;;
+	--source)
+		if [[ -n "$2" && "$2" != -* ]]; then
+			JANK_SOURCE="$2"
+			shift 2
+		else
+			echo "Error: --source requires a value."
+			exit 1
+		fi
+		;;
+	-h | --help)
+		usage
+		;;
+	*)
+		echo "Unknown option: $1"
+		usage
+		;;
+	esac
+done
 
 # Refresh the runtime versions in our nix cache
-NIX_FLAGS="--extra-substituters https://jank-lang.cachix.org \
+NIX_BUILD_FLAGS="--extra-substituters https://jank-lang.cachix.org \
     --extra-trusted-public-keys jank-lang.cachix.org-1:iLjYBD9b/v1D6FxUByF976x3BQ/AJGsQL1Rm49Sw7Fg="
-nix build $NIX_FLAGS --refresh \
-    github:NixOS/nixpkgs/nixos-unstable#clojure \
-    github:NixOS/nixpkgs/nixos-unstable#babashka \
-    git+https://github.com/jank-lang/jank
+
+echo "Populating nix cache with language runtimes"
+nix build $NIX_BUILD_FLAGS --refresh \
+	github:NixOS/nixpkgs/nixos-unstable#clojure \
+	github:NixOS/nixpkgs/nixos-unstable#babashka \
+	$JANK_SOURCE
 
 export IJFY_CLJ_CMD="nix run --offline github:NixOS/nixpkgs/nixos-unstable#clojure --"
 export IJFY_BB_CMD="nix run --offline github:NixOS/nixpkgs/nixos-unstable#babashka --"
-export IJFY_JANK_CMD="nix run --offline git+https://github.com/jank-lang/jank --"
+export IJFY_JANK_CMD="nix run --offline $JANK_SOURCE --"
 
 export IJFY_OUTPUT_DIR="gh-pages-checkout"
 export IJFY_DATA_DIR="$IJFY_OUTPUT_DIR/data"
-
 mkdir -p $IJFY_OUTPUT_DIR $IJFY_DATA_DIR
-python3 harness.py
+
+if [ "$RUN" = true ]; then
+	echo "Running bechmarks..."
+	python3 harness.py
+fi
+
+echo "Rendering website to $IJFY_OUTPUT_DIR"
 quarto render index.qmd --output-dir $IJFY_OUTPUT_DIR --no-clean
 
-pushd $IJFY_OUTPUT_DIR
-git add -f index.html data/*.csv
-git commit -m "Update $(date -u)"
-git push origin gh-pages
-popd
+if [ "$PUBLISH" = true ]; then
+	echo "Publishing to gh-pages"
+
+	pushd $IJFY_OUTPUT_DIR
+	git add -f index.html data/*.csv
+	git commit -m "Update $(date -u)"
+	git push origin gh-pages
+	popd
+fi
