892871b26973b504bdf6edcc69da875f2fb664b7
Kyle Cesare <kcesare@gmail.com>
2026-05-12 12:00:43 -0600

    git commit as automation user.

--- update.sh
+++ update.sh
@@ -89,7 +89,7 @@ if [ "$PUBLISH" = true ]; then
 
 	pushd $IJFY_OUTPUT_DIR
 	git add -f index.html data/*.csv
-	git commit -m "Update $(date -u)"
+	git commit --author "Automation" -m "Update $(date -u)"
 	git push origin gh-pages
 	popd
 fi
